306cb41b180fd3903534003b9cbd1e00b1d9c370
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2018-10-21 21:51:24 -0200

    Capítulo inicial do livro

---
+++ src/main/java/com/rodrigopeleias/capitulo2/Capitulo2.java
@@ -0,0 +1,60 @@
+package com.rodrigopeleias.capitulo2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+class Usuario {
+    private String nome;
+    private int pontos;
+    private boolean moderador;
+
+    public Usuario(String nome, int pontos) {
+        this.nome = nome;
+        this.pontos = pontos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getPontos() {
+        return pontos;
+    }
+
+    public boolean isModerador() {
+        return moderador;
+    }
+
+    public void tornaModerador() {
+        this.moderador = true;
+    }
+}
+
+class Mostrador implements Consumer<Usuario> {
+    public void accept(Usuario usuario) {
+        System.out.println(usuario.getNome());
+    }
+}
+
+public class Capitulo2 {
+
+    public static void main(String[] args) {
+        Usuario user1 = new Usuario("Rodrigo Peleias", 150);
+        Usuario user2 = new Usuario("Denise Gasques", 200);
+        Usuario user3 = new Usuario("Fulano de tal", 300);
+
+        List<Usuario> usuarios = Arrays.asList(user1, user2, user3);
+
+        for (Usuario usuario: usuarios) {
+            System.out.println(usuario.getNome());
+        }
+
+        // java 8 - passando Consumer para forEach
+        usuarios.forEach(new Mostrador());
+
+        // uso direto do Lambda
+        usuarios.forEach(u -> System.out.println(u.getNome()));
+        usuarios.forEach(u -> u.tornaModerador());
+    }
+}
